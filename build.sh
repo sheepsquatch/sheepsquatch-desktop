22a2d5c4d22519b22d267fc5ff07cf2c1176e1a0
sheepsquatch <gabblegobblelol@gmail.com>
2019-08-31 13:16:56 +0200

    Add shit

---
+++ build.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+cd ob-sheepsquatch
+./bootstrap
+./configure --prefix=/usr --sysconfdir=/etc --libdir=/usr/lib64
+make
+sudo make install
+cd ..
+echo "Installing Tint2 Theme...."
+mkdir ~/.config/tint2/
+## Install Tint2 Theme
+cp sheepsquatch.tint2rc ~/.config/tint2/tint2rc
+## Compton
+cp compton.conf ~/.config/compton.conf
+## Install Menu theme
+echo "Installing Start Menu Theme...."
+mkdir ~/.config/jgmenu
+cp sheepsquatchJGRC ~/.config/jgmenu/jgmenurc
+echo "Creating ~/sheep/ dir...."
+## Create Sheep Dir
+mkdir ~/sheep/
+echo "Installing default wallpaper...."
+## Set Original G wallpaper
+cp wall ~/sheep/wallpaper
+feh --bg-scale ~/sheep/wallpaper
+echo "Copying starticon...."
+## Copy fire
+cp sheep.svg ~/sheep/sheep.svg
+##echo "Installing Customizations to openbox...."
+## Install openbox customization
+mkdir -p ~/.config/sheep/
+cp ob-sheepsquatch/data/autostart/autostart ~/.config/openbox/autostart
+echo "Hooking up LOGOUT function to jgmenu...."
+sudo cp sheeplogout.desktop /usr/share/applications/
